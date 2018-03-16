db37a71a7afae69479ddb64059f437b1422b4ae2
Zygal <jnovor@comcast.net>
2018-03-16 16:23:41 -0400

    Add files via upload

---
+++ fuzzyWuzzyWasABear/fuzzyFind.c
@@ -0,0 +1,15 @@
+#include "fuzzyFind.h"
+
+char nameReturn()
+{
+
+  char localNameBuffer[] = "";
+
+
+}
+
+char fuzzyGetName(char* nameBuffer)
+{
+  ///Open a file that has a dictionary of names and maybe even a location in flash where they are stored.
+  ///Each name must have a designated space
+}
--- fuzzyWuzzyWasABear/main.c
+++ fuzzyWuzzyWasABear/main.c
@@ -1,21 +1,25 @@
 //Fuzzy logic for fuzzy bear
 #include "main.h"
+#include "fuzzyFind.h"
 
 int main()
 {
   bool funcTruth = false;
   char nameString[] = "";
+  char username[] = "";
   //Need some type of system control
   printf("Hi!  My name is Fuzzy Wuzzy.  I'm a Von Neumann bear and want to chat with you.\n");
 
   //add an if statement checking to see who the user is
-  if()
+  if(username = fuzzyGetName() == NULL)
   {
-    fuzzyName();
-    printf("");
+    printf("You haven't been here before have you?\n");
+    printf("What's your name?\n");
+    fgets(/*Throw a name in here*/);
   }
   else
   {
-    printf("It seems like you've been here before.\nIsn't your name %s?\n", &nameString);
+    printf("It seems like you've been here before.\nIsn't your name %s?\n", &username);
+    
   }
 }
