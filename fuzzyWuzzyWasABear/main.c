b3bf2125982c3a60b0177b8695fd8080fb128189
Novorita <james.novorita@stryker.com>
2018-03-16 11:32:15 -0400

    Nothing is really here yet...

---
+++ fuzzyWuzzyWasABear/main.c
@@ -0,0 +1,21 @@
+//Fuzzy logic for fuzzy bear
+#include "main.h"
+
+int main()
+{
+  bool funcTruth = false;
+  char nameString[] = "";
+  //Need some type of system control
+  printf("Hi!  My name is Fuzzy Wuzzy.  I'm a Von Neumann bear and want to chat with you.\n");
+
+  //add an if statement checking to see who the user is
+  if()
+  {
+    fuzzyName();
+    printf("");
+  }
+  else
+  {
+    printf("It seems like you've been here before.\nIsn't your name %s?\n", &nameString);
+  }
+}
---
+++ fuzzyWuzzyWasABear/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_   /* Include guard */
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <bool.h>
+/*function prototypes*/
+
+int main(void);
+
+
+#endif // MAIN_H_
